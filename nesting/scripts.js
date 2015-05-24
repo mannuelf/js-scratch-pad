f269d558fe4e51f5e8f0454fe60834aeab510951
Mannuel Ferreira <mannuel@wwc.co.za>
2015-05-24 07:13:59 +0200

    nesting objects 1

---
+++ nesting/scripts.js
@@ -0,0 +1,9 @@
+var box = {};
+
+box.innerBox = {};
+
+box.innerBox.babyBox = {};
+
+box.innerBox['babyBox'];
+
+box.innerBox['babyBox'].says = "Whats up";
---
+++ nesting/scripts2.js
@@ -0,0 +1,14 @@
+var box = {};
+
+// dot notation
+box.innerBox = {};
+box.innerBox2 = {};
+
+// bracket notion
+box['innerBox'] = {};
+
+// bracket + add property
+box['innerBox']['full'] = true;
+box['innerBox2'].empty = false;
+
+
