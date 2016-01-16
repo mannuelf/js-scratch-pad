f2666368bf7dfe74ca4ebfb72cb3a241bdf7efe3
manidf <mannuel.ferreira@gmail.com>
2016-01-16 19:19:32 +0200

    map function

--- eFarmony-Data-Structures_Nesting/scripts.js
+++ eFarmony-Data-Structures_Nesting/scripts.js
@@ -1,10 +1,8 @@
-
-
 // Q1
 var AnimalTest = function (username) {
-   return {
-      username: username
-   }
+	return {
+		username: username
+	}
 };
 
 var myCow = AnimalTest('Bessy');
@@ -13,31 +11,31 @@ console.log(myCow.username);
 // Q2
 
 function AnimalTestUser(username) {
-   var argsLength = arguments.length;
-   var otherArgs = [];
-   if (argsLength > 1) {
-      for (var i = 1; i < argsLength; i++) {
-         otherArgs.push(arguments[i]);
-      }
-   }
+	var argsLength = arguments.length;
+	var otherArgs = [];
+	if (argsLength > 1) {
+		for (var i = 1; i < argsLength; i++) {
+			otherArgs.push(arguments[i]);
+		}
+	}
 
-   return {
-      'username': username,
-      'otherArgs': otherArgs
-   }
+	return {
+		'username': username,
+		'otherArgs': otherArgs
+	}
 }
 
 // Q3
 //Constructor function with 4 paremeters
 function AnimalCreator(username, species, tagline, noises) {
-   var animal = {
-      username: username,
-      species: species,
-      tagline: tagline,
-      noises: noises,
-      friends: []
-   };
-   return animal;
+	var animal = {
+		username: username,
+		species: species,
+		tagline: tagline,
+		noises: noises,
+		friends: []
+	};
+	return animal;
 }
 
 var dog = AnimalCreator('Sam', 'Canine', 'I am barking mad!', ['John', 'Sue', 'Miguel']);
@@ -46,8 +44,8 @@ console.log(dog);
 
 // Q4
 function addFriend(animal, friend) {
-   //animal.freinds.push(friend);
-   animal.friends.push(friend.username);
+	//animal.freinds.push(friend);
+	animal.friends.push(friend.username);
 }
 
 
@@ -59,17 +57,17 @@ console.log(myFarm);
 
 //07
 function addMatchesArray(farm) {
-   for(var animal in farm){
-      farm[animal].matches = [];
-   }
+	for (var animal in farm) {
+		farm[animal].matches = [];
+	}
 }
 addMatchesArray(myFarm);
 console.log(myFarm[0]);
 
-function giveMatches(myFarm){
-   for (var animal in farm) {
-      farm[animal].matches.push(farm[animal].freinds[0]);
-   }
+function giveMatches(myFarm) {
+	for (var animal in farm) {
+		farm[animal].matches.push(farm[animal].freinds[0]);
+	}
 }
 giveMatches(myFarm);
 console.log(myFarm[0]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,40 @@
-var gulp        = require('gulp');
-var gutil       = require('gulp-util');
-var eslint      = require('gulp-eslint');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var reload      = browserSync.reload;
+var eslint = require('gulp-eslint');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var reload = browserSync.reload;
+
+gulp.task('eslint', function() {
+  return gulp.src(['./**/*.js'])
+  .pipe(eslint())
+  .pipe(eslint.result(function(result) {
+    console.log('ESLint result: ' + result.filPath);
+    console.log('# Message: ' + result.messages.length);
+    console.log('# Warning: ' + result.warningCount);
+    console.log('# Errors: ' + result.errorCount);
+  }))
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
+gulp.task('lint', function() {
+  gulp.watch(['./**/*.js'], ['lint']);
+});
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("app/sass/*.scss", ['sass']);
-    gulp.watch("./**/*.html").on('change', reload);
-    gulp.watch("./**/*.js").on('change', reload);
+gulp.task('default', function() {
+  browserSync.init({
+    server: "./"
+  });
+  gulp.watch("app/sass/*.scss", ['sass']);
+  gulp.watch("./**/*.html").on('change', reload);
+  gulp.watch("./**/*.js").on('change', reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/sass/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("app/css"))
-        .pipe(reload({stream: true}));
+  return gulp.src("app/sass/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/css"))
+    .pipe(reload({stream: true}));
 });
