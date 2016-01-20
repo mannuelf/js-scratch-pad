a2ff832d2d41a7164ab978cef977f83c0d0d1f79
manidf <mannuel.ferreira@gmail.com>
2016-01-20 09:41:05 +0200

    local storage example

---
+++ localstorage/js/script.js
@@ -0,0 +1,4 @@
+
+localStorage.clear();
+localStorage.setItem('username', 'Manny');
+localStorage.setItem('country', 'South Africa');
---
+++ localstorage/js/somepage.js
@@ -0,0 +1,2 @@
+
+document.write( '<h1>Hi ' + localStorage.username + ' from ' + localStorage.country + '</h1>');
