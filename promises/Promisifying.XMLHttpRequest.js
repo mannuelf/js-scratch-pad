050d216b673675c6c8e16433ba067310d35fca24
manidf <mannuel.ferreira@gmail.com>
2016-01-23 23:18:19 +0200

    refactor XHR request

--- promises/Promisifying.XMLHttpRequest.js
+++ promises/Promisifying.XMLHttpRequest.js
@@ -1,41 +1,41 @@
 
 function get(url) {
-	// return a new promise.
-	return new Promise(function(resolve, reject) {
-		// Do the usual XHR stuff
-		var req = new XMLHttpRequest();
+	return new Promise(function(resolve, reject) { // return a new promise.
+		var req = new XMLHttpRequest(); // Do the usual XHR stuff
 		req.open('GET', url);
-
 		req.onload = function() {
-			// this is called even on 404 etc
-			// so check the status
-			if (req.status == 200) {
-				// Resolve the promise with the response text
-				resolve(req.response);
+			if (req.status == 200) { // this is called even on 404 etc.  so check the status
+				resolve(req.response); // Resolve the promise with the response text
 			} else {
-				// Otherwise reject with the status text
-				// which will hopefully be a meanngfull error
-				reject(Error(req.statusText));
+				reject(Error(req.statusText)); // Otherwise reject with the status text which will hopefully be a meaningful error
 			}
 		};
-
-		// Handle network errors
-		req.onerror = function() {
+		req.onerror = function() { // Handle network errors
 			reject(Error('Network Error'));
 		};
-
-		// Make the request
-		req.send();
+		req.send(); // Make the request
 	});
 }
 
-//get('story.json').then(function(response) {
+// 01
+// get('story.json').then(function(response) {
 //	console.log('Success!', response);
 //	return JSON.parse(response)
 //}, function(error) {
 //	console.log('Failed!', error);
 //});
 
-get('story.json').then(JSON.parse).then(function(response) {
-	console.log("Yey JSON!", response);
+// 02
+// get('story.json').then(JSON.parse).then(function(response) {
+//	console.log("Yey JSON!", response);
+//});
+
+function getJSON(url) {
+	return get(url).then(JSON.parse);
+}
+
+getJSON('story.json').then(function(story) {
+	return getJSON(story.chapterUrls[0]);
+}).then(function(chapter1) {
+	console.log('Got chapter 1!', chapter1)
 });
