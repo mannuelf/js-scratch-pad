03cd9d6f4aa9200a7bfa48826c96f880122869e3
manidf <mannuel.ferreira@gmail.com>
2016-01-30 19:48:43 +0200

    log messages adjusted

--- object-oriented/prototype.chain.js
+++ object-oriented/prototype.chain.js
@@ -1,18 +1,20 @@
 var gold = { a: 1 }
-console.log(gold.a);
-console.log(gold.z);
+console.log('gold : ', gold.a);
+console.log('gold : ', gold.z);
 
 var blue = (gold);
 // blue.b = 2;
-// console.log(blue.a);
-// console.log(blue.b);
-// console.log(blue.z);
+// console.log('blue : ', blue.a);
+// console.log('blue : ', blue.b);
+// console.log('blue : ', blue.z);
 
 var rose = Object.create(gold);
 rose.b = 2;
-console.log(rose.a);
-console.log(rose.b);
-console.log(rose.z);
+console.log('rose : ', rose.a);
+console.log('rose : ', rose.b);
+console.log('rose : ', rose.z);
 
 gold.z = 3;
-console.log(blue.z);
+console.log('blue : ', blue.z);
+
+console.log('rose : ', rose.z);
