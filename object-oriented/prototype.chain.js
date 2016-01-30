e4511146b5004a4769a21b9b4b6e8ea044ee776e
manidf <mannuel.ferreira@gmail.com>
2016-01-30 19:45:24 +0200

    wip

--- object-oriented/prototype.chain.js
+++ object-oriented/prototype.chain.js
@@ -2,6 +2,17 @@ var gold = { a: 1 }
 console.log(gold.a);
 console.log(gold.z);
 
-var blue = extend({}, gold);
-blue.b = 2;
-console.log(blue.a);
+var blue = (gold);
+// blue.b = 2;
+// console.log(blue.a);
+// console.log(blue.b);
+// console.log(blue.z);
+
+var rose = Object.create(gold);
+rose.b = 2;
+console.log(rose.a);
+console.log(rose.b);
+console.log(rose.z);
+
+gold.z = 3;
+console.log(blue.z);
