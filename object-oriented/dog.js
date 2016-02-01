aee5997302832a35c6c4ac3831f547ffd4ed5365
manidf <mannuel.ferreira@gmail.com>
2016-02-01 08:28:51 +0200

    wip

---
+++ object-oriented/dog.js
@@ -0,0 +1,7 @@
+var Dog = function() {
+    return {leg: 4, bark: alert }
+}
+
+var fido = Dog()
+
+console.log(fido instanceof Dog)
--- object-oriented/run.object.decorator.pattern.js
+++ object-oriented/run.object.decorator.pattern.js
@@ -6,5 +6,6 @@ ben.move()
 
 console.log('Amy drove:', amy.loc)
 console.log('Ben drove:', ben.loc)
-console.log('Car.prototype.constructor')
-console.log('amy.constructor');
+console.log(Car.prototype.constructor)
+console.log(amy.constructor)
+console.log(amy instanceof Car);
