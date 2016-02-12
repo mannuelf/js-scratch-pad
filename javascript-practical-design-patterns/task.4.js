9a55f1d408d17111ded59ce8eb79946559ebdfb2
manidf <mannuel.ferreira@gmail.com>
2016-02-12 05:43:27 +0200

    creating objects 3 ways

--- javascript-practical-design-patterns/task.4.js
+++ javascript-practical-design-patterns/task.4.js
@@ -8,9 +8,22 @@ Object.defineProperty(task, 'toString', {
 		return this.title + ' ' + this.description;
 	},
 	writable: false, // stop you from accidentally overwriting this property
-	enumarable: true,
+	enumarable: false,
 	configurable: false
 });
 
+// inheritance
+var urgentTask = Object.create(task);
 
-console.log( Object.keys(task) );
+Object.defineProperty(urgentTask, 'toString', {
+	value: function () {
+		return this.title + ' ' + 'is urgent';
+	},
+	writable: false,
+	enumarable: false,
+	configurable: false
+});
+
+console.log('TASK', Object.keys(task) );
+console.log('URGENT TASK', Object.keys(urgentTask) );
+console.log('URGENT TASK', urgentTask.toString());
