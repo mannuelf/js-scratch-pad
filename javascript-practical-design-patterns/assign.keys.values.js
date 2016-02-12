baed2cd2318f6aa347c225962eea0fad5c513aed
manidf <mannuel.ferreira@gmail.com>
2016-02-12 05:05:51 +0200

    assigning values

--- javascript-practical-design-patterns/assign.keys.values.js
+++ javascript-practical-design-patterns/assign.keys.values.js
@@ -1,5 +1,16 @@
 var obj = {};
 
-obj.param = 'hello there';
+obj.param = 'hello there, from object one';
 
 console.log(obj.param);
+
+
+var objTwo = {};
+objTwo['param'] = 'I am object two';
+
+console.log(objTwo['param']);
+
+var objThree = {};
+var val = 'Hello from obj three';
+objThree[val] = 'its still me obj three';
+console.log(objThree[val]);
