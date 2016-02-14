aee86096119c01ffad4e13f2659b8218e4996691
manidf <mannuel.ferreira@gmail.com>
2016-02-14 02:05:27 +0200

    revealing module pattern

--- javascript-practical-design-patterns/module.pattern/taskRepository.js
+++ javascript-practical-design-patterns/module.pattern/taskRepository.js
@@ -1,15 +1,23 @@
 var repo = function () {
-	return {
-		get: function (id) {
-			console.log('Getting Task ' + id);
-			return {
-				name: 'new task  from db'
-			}
-		},
-		save: function (task) {
-			console.log('Saving ' + task.name + ' to the db');
+
+	var db = {};
+
+	var get = function (id) {
+		console.log('Getting Task ' + id);
+		return {
+			name: 'new task  from db'
 		}
 	}
+
+	var save = function (task) {
+		console.log('Saving ' + task.name + ' to the db');
+	}
+
+	return {
+		get: ,
+		save:
+	}
+
 }
 
 module.exports = repo();
