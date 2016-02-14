984969f0d1ed3c8a68f95f9c91d5615d8feb17eb
manidf <mannuel.ferreira@gmail.com>
2016-02-14 02:40:51 +0200

    function test

---
+++ jasmine/app.js
@@ -0,0 +1,9 @@
+// JavaScript Testing
+
+// Pass => Expect 2 + 3 = 5
+// Fail => Expect an error if non numbers are passed
+// Fail => Expect 0.1 + 0.2 = 0.3
+
+function add (x, y) {
+	return x + y;
+}
