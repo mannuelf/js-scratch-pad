6ad1c936c8ce3a451857db95e0a18798947f1e9b
Mannuel <mannuel.ferreira@gmail.com>
2016-05-22 08:03:07 +0200

    book application

---
+++ chaining/book.obserbable.properties.js
@@ -0,0 +1,34 @@
+var Book = function (name, price) {
+  this.name = function (val) {
+
+  };
+
+  this.price = function (val) {
+
+  };
+
+  this.onPriceChanging = function (callback) {
+
+  };
+
+  this.onPriceChanged = function (callback) {
+
+  };
+}
+
+var book = new Book('JavaScript: The Good Parts', 233.99);
+
+console.log('The name is: '+ book.name());
+
+console.log('The price is: R' + book.price());
+
+book.onPriceChanging(function, (b, price) {
+  if (price > 100) {
+    console.log('System error, price has gone unexpectedly high');
+    return false;
+  }
+  return true;
+});
+
+book.price(233.99);
+book.price(200);
