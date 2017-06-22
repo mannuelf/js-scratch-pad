920ea87e575e36cc5bbba6e91961fbb968e55ebd
manidf <mannuel.ferreira@gmail.com>
2017-06-22 23:24:14 +0200

    refactor

--- src/functionaljs/flock.seagulls-functional.js
+++ src/functionaljs/flock.seagulls-functional.js
@@ -1,8 +1,8 @@
-var conjoin = function(flock_x, flock_y) {
+var add = function(flock_x, flock_y) {
 	return flock_x + flock_y;
 };
 
-var breed = function(flock_x, flock_y) {
+var multiply = function(flock_x, flock_y) {
 	return flock_x * flock_y;
 };
 
@@ -10,8 +10,8 @@ var flock_a = 4;
 var flock_b = 2;
 var flock_c = 0;
 
-var result = conjoin(
-	breed(flock_b, conjoin(flock_a, flock_c)), breed(flock_a,flock_b)
+var result = add(
+	multiply(flock_b, add(flock_a, flock_c)), multiply(flock_a,flock_b)
 );
 
 console.log(result);
