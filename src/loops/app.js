b7daf8b9ab213242d3fc61038bcc933a8aa3b0cd
manidf <mannuel.ferreira@gmail.com>
2017-07-01 13:01:07 +0200

    modulo loop

--- src/loops/app.js
+++ src/loops/app.js
@@ -7,14 +7,18 @@ while (currentNumber <= 12) {
 
 var result = 1;
 for (var counter = 0; counter < 10; counter = counter +1) {
-	result = result *2;
+	result = result * 2;
 }
 // console.log(result);
 
-
-for (var current = 0; ; current++) {
+// find the first number that is greater than 20 and divisible by 7
+for (var current = 20; ; current++) {
 	if(current % 7 == 0) {
 		break;
 	}
 }
+// console.log(current);
+
+// can also be written also be written as:
+for (var current = 20; current % 7 != 0; current++);
 console.log(current);
