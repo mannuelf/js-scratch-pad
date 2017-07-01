8fe2d2960f622bc388a47d52c7ff4ff2cf8e8120
manidf <mannuel.ferreira@gmail.com>
2017-07-01 14:00:22 +0200

    more functions

--- src/functions/nested-scopes.js
+++ src/functions/nested-scopes.js
@@ -1,3 +1,13 @@
+// The stack
+/*
+    Excerpt from Elequent JavaScript
+    when a function is called, controll is given to the body of that function
+    when that body returns the code that called the function is resumed
+    thus while the body is running the computer must remember the context from which the function was called so that it knows where to continue afterwards.
+    this place where context is stored is known as the stack
+    execution context: means whic function is running, where on the stack is the function running
+*/
+
 // functions create variable scope
 function multiplyAbsolute(number, factor) {
     function multiply(number) {
@@ -10,15 +20,14 @@ function multiplyAbsolute(number, factor) {
     }
 }
 var result = multiplyAbsolute(1,2);
-console.log(result);
+// console.log(result);
 
-// The stack
-/*
-    Excerpt from Elequent JavaScript
-    when a function is called, controll is given to the body of that function
-    when that body returns the code that called the function is resumed
-    thus while the body is running the computer must remember the context from which the function was called so that it knows where to continue afterwards.
-    this place where context is stored is known as the stack
-    execution context: means whic function is running, where on the stack is the function running
+(function chicken() {
+    return egg();
+})();
 
-*/
+function egg() {
+    return chicken();
+}
+
+console.log(chicken() + " came firs.");
