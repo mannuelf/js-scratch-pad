599bd9cbcdc579c995ede23c839e9eaa9327d76f
mannuelf <mannuel.ferreira@gmail.com>
2017-09-22 17:03:29 +0200

    function examples

--- src/functions/first-class-functions.js
+++ src/functions/first-class-functions.js
@@ -7,5 +7,30 @@ var greeting = function(name) {
 }
 
 console.log(hi('Manny F'));
-
 console.log(greeting("Manny"));
+
+/************************************************************/
+
+
+// BAD
+var getServerStuff = function(callback) {
+	return ajaxCall(function(json) {
+		return callback(json);
+	});
+};
+
+// GOOD
+var getServerStuff = ajaxCall;
+
+/************************************************************/
+
+// This line
+return ajaxCall(function(json) {
+	return callback(json);
+});
+
+// is the same as this line
+return ajaxCall(callback);
+
+// which isequivalent to this
+var getServerStuff = ajaxCall;
