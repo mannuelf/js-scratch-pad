4f9ee84573f893531ab8e70d3b4290e724f7512d
mannuelf <mannuel.ferreira@gmail.com>
2017-09-23 16:36:39 +0200

    es5 version of a user constructor and update method in use

---
+++ src/classes/app.js
@@ -0,0 +1,24 @@
+function User(username, email) {
+	this.username = username;
+	this.email = email;
+
+	/*
+	this.changeEmail = function () {
+		   Do not usea function like this not performent.
+		   setting function here will redeclare and redefined for every single instance of console-adjacent-user-command-result
+		   instead it is better to attach this method to the prototype so that each new instance of User will share this changeEmail method
+	}
+	*/
+}
+
+// attach to the prototype so that every instance of User will share the changeEmail method.
+User.prototype.changeEmail = function(newEmail) {
+	this.email = newEmail;
+}
+
+var mannuel = new User('Mannuel Ferreira', 'mannuel@email.com');
+
+mannuel.changeEmail('manny@email.com');
+
+console.dir(mannuel);
+
