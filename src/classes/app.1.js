da402cbbbb1adf38289a2ce4dfc860192ea76b3e
mannuelf <mannuel.ferreira@gmail.com>
2017-09-23 16:55:32 +0200

    using setters and getters

--- src/classes/app.1.js
+++ src/classes/app.1.js
@@ -5,14 +5,36 @@ class User {
 		this.email = email;
 	}
 
+	// static method,using spread operator
+	static register(...args) {
+		console.log(args); // any arguments passed in using spread operator will be an array
+		return new User(...args);
+	}
+
 	changeEmail(newEmail) {
 		this.email = newEmail;
 	}
 
+	// accessors. setters and getters.
+
+	get foo() {
+		return 'foo';
+	}
+
+	set bar() {
+		return 'bar';
+	}
+
 }
 
+// call directly on constructor
 let mannuel = new User('Mannuel Ferreira', 'mannuel@email.com');
 
+// call with static method
+let joey = User.register('Joey Jammer', 'Joey@email.com');
+
 mannuel.changeEmail('MannyF@email.co.za');
+
 console.dir(mannuel);
+console.dir(mannuel.foo);
 
