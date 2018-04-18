7d0b08e1e059b0a371c34dc39970d68bbed5bda6
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-04-18 23:58:11 +0200

    number greater than 50 filter

---
+++ src/numbersFilter.js
@@ -0,0 +1,10 @@
+// filter numbers greater than fifty
+var numbers = [15, 25, 35, 45, 55, 65, 75, 85, 95];
+
+var filteredNumbers;
+
+var filteredNumbers = numbers.filter((number) => {
+   return number > 50;
+});
+
+filteredNumbers;
