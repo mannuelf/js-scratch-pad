7bc4e05d10117229a0f2f03151f0429a9ead1fea
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-05-20 11:50:28 +0200

    spread operator

--- src/rest.and.spread.4.js
+++ src/rest.and.spread.4.js
@@ -1,3 +1,14 @@
 function join(array1, array2) {
 	return [...array1, ...array2];
 }
+
+function unshift(array, ...numbers) {
+	return [...numbers, ...array];
+}
+   
+unshift([1,2,3,4,5], 1,2,3,4,5,6,7);
+
+function unshift(array, ...numbers) {
+	return numbers.concat(array)
+}
+unshift([1,2,3,4,5], 1,2,3,4,5,6,7);
