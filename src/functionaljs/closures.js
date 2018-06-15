c5d3327e08a1b49929d5d8f620d214e48d6c51f6
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-06-15 06:04:31 +0200

    testing

--- src/functionaljs/closures.js
+++ src/functionaljs/closures.js
@@ -2,10 +2,11 @@
 // functions are closures, function body has access to variables that are defined outside the function.
 
 var me = 'Mannuel Ferreira';
-
+greetMe();
 function greetMe() {
     console.log('Hello, ' + me + '!');
 }
 
 me = 'Batman';
+me = 'Radman';
 greetMe();
