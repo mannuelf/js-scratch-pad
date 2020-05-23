2781539130ce0de7fa03b9dea5391ce35550ebea
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-05-23 21:50:51 +0200

    react react react

--- src/counter-app/src/components/counter.jsx
+++ src/counter-app/src/components/counter.jsx
@@ -1,11 +1,31 @@
-import React, { Component } from "react";
+import React, {Component, Fragment} from "react";
 
 class Counter extends Component {
 	state = {
 		count: 0,
-		tags: ["tag1", "tag2", "tag3"],
+		tags: ["tag1", "tag2"],
 		imageUrl: "https://picsum.photos/200"
 	};
+
+	//constructor() {
+	//	super();
+	//	this.handleIncrement = this.handleIncrement().bind(this);
+	//}
+
+	renderTags() {
+		if (this.state.tags.length === 0) return <p>There are no tags</p>;
+		return <ul>{this.state.tags.map((tag, index) => <li key={index}>{tag}</li>)}</ul>
+	}
+
+	handleIncrement = () => {
+		this.setState({count: this.state.count + 1});
+	}
+
+	doHandleIncrement = (product) => {
+		// dont build wrapper functions, rather do inline ones L38
+		console.log(product)
+		this.handleIncrement({id: 1});
+	}
 
 	render() {
 		return (
@@ -13,15 +33,17 @@ class Counter extends Component {
 				<span className={this.getBadgeClasses()}>
 					{this.formatCount()}
 				</span>
-				<button className="btn btn-secondary btn-sm">Increment</button>
 
-				<ul>
-					{
-						this.state.tags.map((tag, index) =>
-							<li key={index}>{tag}</li>
-						)
-					}
-				</ul>
+				<button
+					onClick={(e) => this.handleIncrement(product)}
+					className="btn btn-secondary btn-sm"
+				>
+					Increment
+				</button>
+
+				{this.state.tags.length === 0 && 'Please create a new tag'}
+
+				{this.renderTags()}
 			</div>
 		);
 	}
@@ -33,7 +55,7 @@ class Counter extends Component {
 	}
 
 	formatCount() {
-		const { count } = this.state;
+		const {count} = this.state;
 		return count === 0 ? "Zero" : count;
 	}
 }
