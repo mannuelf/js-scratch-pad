234f16626a09d3509298048a30fd7baaa9f9ebd5
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-07-03 09:03:25 +0200

    either

--- src/compositional-functional-js/03_either-color.js
+++ src/compositional-functional-js/03_either-color.js
@@ -12,11 +12,16 @@ const Left = x => ({
   inspect: () => `Left(${x})`
 });
 
-const result = Left(2)
-  .map(x => x + 1)
-  .map(x => x / 2)
+const fromNullable = x => (x != null ? Right(x) : Left(null));
+
+const findColor = name =>
+  fromNullable({ red: "#ff4444", blue: "#3b5998", yellow: "#fff68f" }[name]);
+
+const result = findColor("red")
+  .map(c => c.slice(1))
   .fold(
-    x => "error",
-    x => x
+    e => "no color",
+    c => c.toUpperCase()
   );
+
 console.log(result);
